7cb37cbf0ecfbcec7bb42722040a5a3510fdebfe
Sainathan V <sainathan.sssihl@gmail.com>
2026-07-16 21:09:39 +0530

    py_day_1

--- 1. The Bouncer System.py
+++ 1. The Bouncer System.py
@@ -1,5 +1,17 @@
 # 1. The Bouncer System
+'''Day 1: The Bouncer System
 
+Scenario: You are programming the digital entry gate for a restricted venue. The gate needs to automatically evaluate guests based on their age before letting them pass.
+Rules:
+
+    If the user's age is less than 18, print "Access Denied: Underage."
+
+    If the user's age is between 18 and 21 (inclusive), print "Access Granted: Limited Pass (No alcohol allowed)."
+
+    If the user's age is greater than 21, print "Access Granted: Full Access Pass."
+
+    Implement data type safety handling to prevent the program from crashing if a user inputs text instead of digits.
+'''
 def calculate_age(age):
     if age < 0:
         return "Invalid Age"
