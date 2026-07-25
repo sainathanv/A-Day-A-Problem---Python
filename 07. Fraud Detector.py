62f3e0ae9af49bdca8f2372ff8947097751b35b3
Sainathan V <sainathan.sssihl@gmail.com>
2026-07-25 15:59:57 +0530

    py_day_07

--- 07. Fraud Detector.py
+++ 07. Fraud Detector.py
@@ -8,7 +8,6 @@ Rules:
     Critical Threshold: If any transaction is greater than $1,000, print a critical alert, set fraud_detected = True, increment the counter, and immediately break the loop.
     Warning Threshold: If a transaction is between $500 and $1,000 (inclusive), print a warning alert, set fraud_detected = True, increment the counter, and continue scanning.
     Safe Path: For transactions under $500, print a clear message.
-
     Final Evaluation: After the loop, check the flag/counter. If fraud was detected, print "BATCH AUDIT FAILED: Account locked pending review." Otherwise, print "BATCH AUDIT SUCCESSFUL: All transactions cleared."
 '''
 
